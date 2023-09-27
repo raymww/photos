6b12c840ed92d389412f53a4ea0fbc16ac86cbda
raymww <raymond.m.wang@gmail.com>
2023-09-27 13:19:58 -0500

    done

---
+++ assets/js/scripts.js
@@ -0,0 +1,10 @@
+
+function pictureClickHandler(){
+    var gallery = document.getElementById("photo-container-container");
+    var fullImage = document.getElementById("max-container");
+
+    // change image source to be based on calling element with map
+    
+    gallery.style.visibility = 'hidden';
+    fullImage.style.visibility = 'visible';
+}
